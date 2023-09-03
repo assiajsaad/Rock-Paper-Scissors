230c6d5a0055f669d8b01cc161fa53bd07228928
Assia Saad <assia.j.saad@gmail.com>
2023-09-03 13:05:06 +0300

    ..

--- scripts.js
+++ scripts.js
@@ -27,39 +27,54 @@ function playerSelection() {
     return playerChoice;
 }
 
+function addScore(player, computer) {
+    playerScore += player;
+    computerScore += computer;
+
+    let pScore = document.querySelector('#playerScore');
+    let cScore = document.querySelector('#computerScore');
+
+    pScore.textContent = playerScore;
+    cScore.textContent = computerScore;
+}
+function canContinue() {
+    if (playerScore >= 5 || computerScore >= 5) {
+        return false;
+    }
+}
+
 function playRound(playerChoice, computerChoice) {
     let result;
-    if (playerChoice === computerChoice) {
 
+    if (playerChoice === computerChoice) {
+        addScore(0, 0);
         result = `It's Draw! ${playerChoice} is equal to ${computerChoice}`;
 
     } else if (playerChoice === "Rock" && computerChoice === "Paper") {
-
+        addScore(0, 1);
         result = `You Lose ! ${computerChoice} beats ${playerChoice}`;
 
     } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
-
+        addScore(0, 1);
         result = `You Lose ! ${computerChoice} beats ${playerChoice}`;
     }
     else if (playerChoice === "Scissors" && computerChoice === "Rock") {
-
+        addScore(0, 1);
         result = `You Lose ! ${computerChoice} beats ${playerChoice}`;
 
     } else if (playerChoice === "Paper" && computerChoice === "Rock") {
-
+        addScore(1, 0);
         result = `You Win ! ${playerChoice} beats ${computerChoice}`;
 
     } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
-
+        addScore(1, 0);
         result = `You Win ! ${playerChoice} beats ${computerChoice}`;
     }
     else if (playerChoice === "Rock" && computerChoice === "Scissors") {
-
+        addScore(1, 0);
         result = `You Win ! ${playerChoice} beats ${computerChoice}`;
     }
-    // console.log("Player:"+ playerChoice);
-    // console.log("CPU: "+ computerChoice);
-    // console.log(result);
+
     return result;
 
 }
@@ -73,4 +88,36 @@ function game() {
     }
 }
 
-game();
+var playerScore = 0;
+var computerScore = 0;
+
+let pScore = document.querySelector('#playerScore');
+let cScore = document.querySelector('#computerScore');
+
+pScore.textContent = playerScore;
+cScore.textContent = computerScore;
+
+let buttons = document.querySelectorAll('.choice');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+
+        let result = document.querySelector('.result');
+
+        result.innerHTML = playRound(button.textContent, getComputerChoice());
+    })
+})
+
+let reset = document.querySelector("#reset");
+reset.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+
+    pScore.innerHTML = playerScore;
+    cScore.innerHTML = computerScore;
+
+    let result = document.querySelector('.result');
+
+    result.innerHTML = '';
+})
+
